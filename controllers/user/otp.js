80ee82589aa83e12edc0cdbd0f112cbd1ab5b498
Trương Nhật Ninh <blackpanthertnn@gmail.com>
2020-08-02 22:11:30 +0700

    Qrcode Get OTP

--- controllers/user/otp.js
+++ controllers/user/otp.js
@@ -1,13 +1,27 @@
 const speakeasy = require('speakeasy');
 const qrocde = require('qrcode');
+var asciii = '';
+var secret = '';
 module.exports.get = async(req, res) => {
     const curentUser = req.curentUser;
 
-    var secret = speakeasy.generateSecret({
+    secret = speakeasy.generateSecret({
         name: curentUser.name
     })
     qrocde.toDataURL(secret.otpauth_url, function(err, data) {
         res.render('user/otp', { data });
     })
+}
 
+module.exports.post = async(req, res) => {
+    var verify = speakeasy.totp.verify({
+        secret: asciii,
+        encoding: 'base32',
+        token: req.body.otp
+    });
+    if (verify == true) {
+        //nếu mã otp đúng thì trả về...
+    } else {
+        //nếu otp sai thì trả về gì đó...
+    }
 }
