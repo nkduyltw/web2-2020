862e682bcba039334ad45a2971cf130778b77627
Nguyễn Khánh Duy <nkduy17ck1@gmail.com>
2020-08-12 12:53:41 +0700

    fix db

--- services/db.js
+++ services/db.js
@@ -1,9 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: 'postgres',
-});
+const db = new Sequelize(process.env.DATABASE_URL);
 
 
 module.exports = db;
