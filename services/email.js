f3a3bda2916a7cc980173532d7b0a14bff47361c
Nguyễn Khánh Duy <nkduy17ck1@gmail.com>
2020-08-24 16:59:09 +0700

    update

--- services/email.js
+++ services/email.js
@@ -1,15 +1,17 @@
 const nodemailer = require('nodemailer');
 
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+    }
+});
+
 async function send(to, subject, content) {
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD,
-        }
-    });
+    
 
     return transporter.sendMail({
         from: process.env.EMAIL_USERNAME,
